3e98b11df999d81d5508c94958adfbad7612a91d
hasnainnaqa <muhammedhasnain033@gmail.com>
2025-08-07 16:29:15 +0500

    payroll edit

--- src/components/Payroll/PayrollCard.jsx
+++ src/components/Payroll/PayrollCard.jsx
@@ -8,14 +8,15 @@ const PayrollCard = ({ payment }) => {
   const { t } = useTranslation();
   if (!payment) return null;
 
-  const addedUsers = payment.usersAdded 
-
+  const addedUsers = payment.usersAdded;
+  const selectedEmployees = addedUsers
   return (
     <div className="bg-[#FFFFFF] rounded-[32px] p-6 relative">
-      <button className="absolute top-6 right-6 bg-[#54F439] p-3 rounded-full hover:bg-[#52ff34] transition w-10 h-10">
-        <Pencil size={16} className="text-[#020500] " />
-      </button>
-
+      <Link to="/payroll/createtransaction" state={{ selectedEmployees }}>
+        <button className="absolute top-6 right-6 bg-[#54F439] p-3 rounded-full hover:bg-[#52ff34] transition w-10 h-10">
+          <Pencil size={16} className="text-[#020500] " />
+        </button>
+      </Link>
       <div className="flex justify-center">
         <div className="w-[85px] h-[85px] bg-gray-100 rounded-full flex items-center justify-center">
           <Clipboard className="w-10 h-10 text-[#575757]" />
@@ -50,29 +51,32 @@ const PayrollCard = ({ payment }) => {
           <h4 className="text-sm text-[#020500] font-medium">
             {t("Added Users")}
           </h4>
-          {addedUsers.length>6 &&
-              <Link
-                to="/payroll/users"
-                state={{ addedUsers }}
-                className="text-[#21A90A] text-sm font-medium hover:underline">
-                {t("View All")}
-              </Link>
-          }
+          {addedUsers.length > 6 && (
+            <Link
+              to="/payroll/users"
+              state={{ addedUsers }}
+              className="text-[#21A90A] text-sm font-medium hover:underline">
+              {t("View All")}
+            </Link>
+          )}
         </div>
 
         <div className="mt-4 overflow-hidden">
-          {addedUsers.map((user, index) => index < 6 && (
-            <div
-              key={index}
-              className="flex items-center gap-3 p-3 h-15 border-b border-[#EAECF0]  hover:bg-gray-100">
-              <img
-                src={user.img}
-                alt={user.name}
-                className="w-8 h-8 rounded-full object-cover"
-              />
-              <span className="text-sm text-[#020500]">{user.name}</span>
-            </div>
-          ))}
+          {addedUsers.map(
+            (user, index) =>
+              index < 6 && (
+                <div
+                  key={index}
+                  className="flex items-center gap-3 p-3 h-15 border-b border-[#EAECF0]  hover:bg-gray-100">
+                  <img
+                    src={user.img}
+                    alt={user.name}
+                    className="w-8 h-8 rounded-full object-cover"
+                  />
+                  <span className="text-sm text-[#020500]">{user.name}</span>
+                </div>
+              )
+          )}
         </div>
       </div>
     </div>
