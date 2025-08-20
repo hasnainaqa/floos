e088a03de9c8fe864169682cb84da9a2ce8197d1
hasnainnaqa <muhammedhasnain033@gmail.com>
2025-08-20 18:36:02 +0500

    baseURL

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -9,8 +9,10 @@ const CheckoutPage = () => {
   const [status, setStatus] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const BASE_URL = "http://213.232.203.198:3000"; // e.g., http://213.232.203.198:3000
-
+  const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"      // local backend
+    : "/api"; 
   useEffect(() => {
     const token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30";
@@ -28,7 +30,7 @@ const CheckoutPage = () => {
         setError(err.response?.data || "Error fetching invoice");
         setLoading(false);
       });
-  }, [invoiceId]);
+  }, [invoiceId, BASE_URL ]);
 
   return (
     <div className="rounded-[48px] sm:p-6 bg-white">
